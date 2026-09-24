d42d6f487b3bc09dc25aa7b3aa8372853507aac9
Nelson de Sousa Soares <nelson.soares@netcracker.com>
2026-09-24 16:49:09 -0300

    updates

--- bookshop-frontend/src/app/components/book-list/book-list.component.ts
+++ bookshop-frontend/src/app/components/book-list/book-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Observable, Subject, Subscription, debounceTime, distinctUntilChanged } from 'rxjs';
